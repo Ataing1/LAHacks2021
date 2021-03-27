2a42f2b4b0fbe55b09539e4c207acbdbcfbe2382
Daniel Koo <donghyun122600@gmail.com>
2021-03-27 13:53:23 -0700

    Created the student's view of the application

---
+++ pages/student.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import styles from '../styles/student.module.css'
+
+export default function Test() {
+    return (
+        // should it be a html video element?
+        <div>
+            <div className={styles.teacherPanel}>
+                <p className={styles.userName}>Professor</p>
+            </div>
+
+            <div className={styles.row}>
+                <div className={styles.user}>
+                    <p className={styles.userName}>Amy</p>
+                </div>
+                <div className={styles.user}>
+                    <p className={styles.userName}>Ben</p>
+                </div>
+                <div className={styles.user}>
+                    <p className={styles.userName}>Cain</p>
+                </div>
+                <div className={styles.user}>
+                    <p className={styles.userName}>David</p>
+                </div>
+            </div>
+
+            <div className={styles.toolbar}>
+                <button className={styles.action}>shout</button>
+                <button className={styles.action}>mute</button>
+                <button className={styles.action}>chat</button>
+                <button className={styles.action}>switch</button>
+                <button className={styles.leave}>leave</button>
+            </div>
+        </div>
+    )
+}
